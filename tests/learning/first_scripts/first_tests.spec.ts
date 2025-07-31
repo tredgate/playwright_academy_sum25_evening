53f8e3ebc96c7f8a914b415200ea51c355725a3d
MilanPavelcak <milan.pavelcak@sinnerschrader.com>
2025-07-31 20:51:03 +0200

    Install playwright and add first test

---
+++ tests/learning/first_scripts/first_tests.spec.ts
@@ -0,0 +1,9 @@
+import { test } from "@playwright/test";
+
+test("First test", async ({ page }) => {
+  //test code
+  await page.goto("http://tredgate.com/pmtool/");
+  await page.locator("#username").fill("pw_academy");
+  await page.locator("#password").fill("Playwright321!");
+  await page.locator(".btn").click();
+});
